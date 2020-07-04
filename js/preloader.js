61b48f681a7efac865936ce5a20d8dd84ede8623
AlexBazh <bazhenov570@gmail.com>
2020-07-05 02:25:16 +0700

    fixed preloader

--- js/preloader.js
+++ js/preloader.js
@@ -3,4 +3,7 @@ let preloader = document.querySelector('.preloader');
 
 window.onload = function (){
     preloader.classList.add('hidePreloader');
-}
+    window.setTimeout(function(){
+        preloader.classList.add('removePreloader');
+    }, 1000);
+};
