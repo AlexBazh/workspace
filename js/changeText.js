aee9f4f728ac53bd3544b9b572e5b252b2728ddc
Liana <liana@MacBook-Pro-Aleksandr-2.local>
2020-07-04 13:14:00 +0700

    fixed changeText

--- js/changeText.js
+++ js/changeText.js
@@ -3,7 +3,6 @@ let changeForm = document.querySelector('.changeForm');
 let buttonChange = document.querySelector('.buttonChange');
 
 buttonChange.onclick = function (evt) {
-	console.log('1');
 	evt.preventDefault ();
 	changeText.textContent = changeForm.value;
 };
