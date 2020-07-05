4c7a30f5e2a93f510b3de8173355838f80c603bf
AlexBazh <bazhenov570@gmail.com>
2020-07-05 19:36:25 +0700

    added sw

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 feather.replace()
+
+window.addEventListener('load', () => {
+
+    if ('serviceWorker' in navigator){
+
+        navigator.serviceWorker.register('sw.js')
+            .then(registration => {
+                console.log('Service worker successfully registered', registration);
+            })
+            .catch(error => {
+                console.log('Service worker registration failed', error);
+            });
+    }
+
+});
