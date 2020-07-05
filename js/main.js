39bde0948e826e6725a3c38aaa20b60249f6cd13
AlexBazh <bazhenov570@gmail.com>
2020-07-05 19:43:56 +0700

    fixed sw

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ window.addEventListener('load', () => {
 
     if ('serviceWorker' in navigator){
 
-        navigator.serviceWorker.register('sw.js')
+        navigator.serviceWorker.register('./sw.js')
             .then(registration => {
                 console.log('Service worker successfully registered', registration);
             })
