1e097a7766d03f3ae064b266f950147eace84e07
AlexBazh <bazhenov570@gmail.com>
2020-07-05 21:02:18 +0700

    added caches

--- sw.js
+++ sw.js
@@ -1,4 +1,33 @@
+const staticCacheName = 'static-cache-v0';
+
+const staticAssets = [
+    './',
+    './index.html',
+    './styles/main.css',
+    './img/preloaderLogo.svg',
+    './img/Main/Fab/icon/20/send.svg',
+    'https://unpkg.com/feather-icons',
+    './js/main.js',
+    './js/changeText.js',
+    './js/likes.js',
+    './js/createTask.js',
+    './js/preloader.js',
+    './styles/grid.css',
+    './styles/typography.css',
+    './styles/colors.css',
+    './styles/shadows.css',
+    './styles/buttons.css',
+    './styles/inputs.css',
+    './styles/fonts.css',
+    './styles/darkTheame.css',
+    './img/Main/Task/icon/20/chevron-right.svg',
+    './fonts/Circe-Bold.woff',
+    './fonts/Circe-Regular.woff'
+];
+
 self.addEventListener('install', async event => {
+    const cache = await caches.open(staticCacheName);
+    await cache.addAll(staticAssets);
     console.log('Service worker has been installed');
 });
 
