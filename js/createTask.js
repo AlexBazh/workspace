b518b7dee9db5b3ce369190c5105c40c167e5924
Liana <pla_alp@bk.ru>
2020-07-06 22:06:46 +0700

    added filter, fixed limitCounter

--- js/createTask.js
+++ js/createTask.js
@@ -3,11 +3,15 @@ let createTaskTextarea = document.querySelector('.createTaskTextarea');
 let priorityTaskButton = document.querySelector('.priorityTaskButton');
 let createTaskButton = document.querySelector('.createTaskButton');
 let listTask = document.querySelector('.listTask');
-
-
+// переменные для лимита символов
 let charCounter = document.querySelector('.charCounter');
 let textCounter = document.querySelector('.textCounter');
+// переменные для фильтра
+let filter = document.querySelector('.filter');
+let tasks = document.getElementById('testTask');
+let childTasks = tasks.children;
 
+// кнопка приоритета
 priorityTaskButton.onclick = function () {
 	priorityTaskButton.classList.toggle('important_btn');
 	if (priorityTaskButton.classList.contains('important_btn')) {
@@ -15,31 +19,58 @@ priorityTaskButton.onclick = function () {
 	} else {
 		priorityTaskButton.textContent = 'Обычная задача';
 	};
-
 };
 
+//отправка формы (создание новой задачи)
 createTaskForm.onsubmit = function (evt) {
 	evt.preventDefault();
 	let newTask = document.createElement('li');
 	listTask.append(newTask);
+	newTask.classList.add('tasks'); //добавляем класс tasks ко всем новым задачам
 	newTask.textContent = createTaskTextarea.value;
 	if (priorityTaskButton.classList.contains('important_btn')) {
 		newTask.classList.add('important_task');
+		newTask.dataset.category = 'importants';
+	} else {
+		newTask.dataset.category = 'regulars';
 	};
 	createTaskTextarea.value = '';
-	charCounter.textContent = 0;
+	charCounter.textContent = 0; // обнуляем значение введенных символов после отправки формы 
+	textCounter.classList.remove('textCounterWarning');
 };
 
+//лимит символов для создания текста задачи 
 createTaskTextarea.oninput = function () {
  	charCounter.textContent = createTaskTextarea.value.length;
 
- 	if (createTaskTextarea.value.length>40) {
- 		createTaskButton.disabled = true;
+ 	if (createTaskTextarea.value.length>=40) {
+ 		createTaskTextarea.setAttribute("maxlength", "40")
 		textCounter.classList.add('textCounterWarning');
  	} else { 
- 		createTaskButton.disabled = false;
 		textCounter.classList.remove('textCounterWarning');
  	};
-
 };
+
+//фильтр
+
+filter.onchange = function () {
+for (let childTask of childTasks) {
+	if (childTask.dataset.category !== filter.value && filter.value !== 'all') {
+		childTask.classList.add('hidden');
+	} else {
+		childTask.classList.remove('hidden');
+	};
+};	
+};
+
+
+
+
+
+
+
+
+
+
+
 
