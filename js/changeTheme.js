9c71a9dffdec3868d4ac8760a968ab9d38d1b0f4
Liana <pla_alp@bk.ru>
2020-07-11 16:37:23 +0700

    added dark theme

---
+++ js/changeTheme.js
@@ -0,0 +1,13 @@
+let bodyChangeTheme = document.querySelector('body');
+let buttonChangeTheme = document.querySelector('.buttonChangeTheme');
+let spanChangeTheme = document.querySelector('.spanChangeTheme');
+let iChangeTheme =document.querySelector('.iChangeTheme');
+
+
+
+buttonChangeTheme.onclick = function () {
+	bodyChangeTheme.classList.toggle('dark');
+	bodyChangeTheme.classList.toggle('light');
+	buttonChangeTheme.classList.toggle('primaryS_dark');
+	spanChangeTheme.textContent = "Светлая тема";
+}
